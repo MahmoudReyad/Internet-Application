c06524286e8a8fa56b8e002b1ab8b78cc6c5084a
Mohamed Abd EL-Wahid <i.mohamed.abd.elwahid@gmail.com>
2018-04-18 17:24:50 +0200

    add project files

---
+++ App_01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(App_01.Startup))]
+namespace App_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
